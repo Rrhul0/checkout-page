586948c1e6e35de194c17b06dc7541398d503228
Rahul Raj <rahulraj1417@gmail.com>
2023-07-12 20:24:27 +0530

    fixed header by adding back button for small screen devices

--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,26 @@
 import logo from '@/public/logo.svg'
 import questionMark from '@/public/question-mark.svg'
 import Image from 'next/image'
+import backImage from '@/public/back.svg'
 
 export default function Header() {
 	return (
-		<header className='flex justify-between py-6'>
+		<header className='flex justify-between py-5 md:py-6 px-6 sm:px-16 md:px-0'>
 			<Image
 				src={logo}
 				alt='tickete'
 				width={76}
 				height={24}
 				priority
+				className='hidden md:block'
 			/>
-			<div className='flex gap-1'>
+			<Image
+				src={backImage}
+				alt='back'
+				width={28}
+				height={28}
+			/>
+			<div className='gap-1 hidden md:flex'>
 				<Image
 					src={questionMark}
 					alt={'?'}
